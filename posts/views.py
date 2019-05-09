cd8ce6a397f00352f297627a2d19ac2c597465b6
noxevolution <proloyd4@gmail.com>
2019-05-10 00:56:52 +0530

    pagination done in html

--- posts/views.py
+++ posts/views.py
@@ -22,7 +22,7 @@ def index(request):
 
 def blog(request):
     post_list = Post.objects.all()
-    paginator = Paginator(post_list, 1)
+    paginator = Paginator(post_list, 4)
     page_request_var = 'page'
     page = request.GET.get(page_request_var)
     
@@ -34,7 +34,7 @@ def blog(request):
         paginated_queryset = paginator.page(paginator.num_pages)
     
     context = {
-        'post_list': paginated_queryset,
+        'queryset': paginated_queryset,
         'post_request_var': page_request_var
     }
     
