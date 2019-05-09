61f8ec68f28b39361551526f51807d70c70ba768
noxevolution <proloyd4@gmail.com>
2019-05-10 01:28:38 +0530

    data rendered in blog page & latest posts"

--- posts/views.py
+++ posts/views.py
@@ -21,6 +21,7 @@ def index(request):
     return render(request, 'index.html', context)
 
 def blog(request):
+    most_recent = Post.objects.order_by('-timestamp')[:3]
     post_list = Post.objects.all()
     paginator = Paginator(post_list, 4)
     page_request_var = 'page'
@@ -35,10 +36,11 @@ def blog(request):
     
     context = {
         'queryset': paginated_queryset,
-        'post_request_var': page_request_var
+        'post_request_var': page_request_var,
+        'most_recent': most_recent
     }
     
     return render(request, 'blog.html', context)
 
-def post(request):
+def post(request, id):
     return render(request, 'post.html', {})
