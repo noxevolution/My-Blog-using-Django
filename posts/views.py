98f2eeefd42473f22f9f2b47fbbd9adf2fd68797
noxevolution <proloyd4@gmail.com>
2019-05-11 00:56:36 +0530

    Tags & Articles regarding tags

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,12 @@
+from django.db.models import Count
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 from .models import Post
 from marketing.models import Signup
+
+def get_category_count():
+    queryset = Post.objects.values('categories__title').annotate(Count('categories__title'))
+    return queryset
 
 def index(request):
     featured = Post.objects.filter(featured=True)
@@ -21,6 +26,7 @@ def index(request):
     return render(request, 'index.html', context)
 
 def blog(request):
+    category_count = get_category_count()
     most_recent = Post.objects.order_by('-timestamp')[:3]
     post_list = Post.objects.all()
     paginator = Paginator(post_list, 4)
@@ -37,7 +43,8 @@ def blog(request):
     context = {
         'queryset': paginated_queryset,
         'post_request_var': page_request_var,
-        'most_recent': most_recent
+        'most_recent': most_recent,
+        'category_count': category_count
     }
     
     return render(request, 'blog.html', context)
