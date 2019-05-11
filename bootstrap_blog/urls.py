0219d05c331d40e37d4366f03aa1db41afb8a1d8
noxevolution <proloyd4@gmail.com>
2019-05-12 02:44:12 +0530

    Search working now in blog listing page

--- bootstrap_blog/urls.py
+++ bootstrap_blog/urls.py
@@ -17,12 +17,13 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
-from posts.views import index, blog, post
+from posts.views import index, blog, post, search
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
     path('blog/', blog, name="post-list"),
+    path('search/', search, name="search"),
     path('post/<id>/', post, name="post-detail"),
 ]
 
