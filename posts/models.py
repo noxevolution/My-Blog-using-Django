80edfbec49c05deb9659ca1f3bbd458cedab1dcf
noxevolution <proloyd4@gmail.com>
2019-05-18 00:21:03 +0530

    commenting form done below post

--- posts/models.py
+++ posts/models.py
@@ -42,4 +42,13 @@ class Post(models.Model):
             'id': self.id
         })
 
+class Comment(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
 
