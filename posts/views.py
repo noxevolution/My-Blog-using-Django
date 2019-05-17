5efca870df57f9435cd3df3d8b4ca9f658a9aff4
noxevolution <proloyd4@gmail.com>
2019-05-17 23:32:48 +0530

    sidebar is now separated & stated working on comments

--- posts/views.py
+++ posts/views.py
@@ -79,8 +79,13 @@ def blog(request):
     return render(request, 'blog.html', context)
 
 def post(request, id):
+    category_count = get_category_count()
+    most_recent = Post.objects.order_by('-timestamp')[:3]
     post = get_object_or_404(Post, id=id)
+    
     context = {
         'post': post,
+        'most_recent': most_recent,
+        'category_count': category_count
     }
     return render(request, 'post.html', context)
