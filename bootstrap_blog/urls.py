6eddb1ca37d8fe488df1a281b049f80cf7f73d59
noxevolution <proloyd4@gmail.com>
2019-06-07 01:43:32 +0530

    Post create view set up!

--- bootstrap_blog/urls.py
+++ bootstrap_blog/urls.py
@@ -17,16 +17,19 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
-from posts.views import index, blog, post, search
+from posts.views import ( 
+     index, blog, post, search, 
+     post_create, post_update, post_delete)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
     path('blog/', blog, name="post-list"),
     path('search/', search, name="search"),
+    path('create/', post_create, name="post-create"),
     path('post/<id>/', post, name="post-detail"),
-    path('post/<id>/update/', post, name="post-update"),
-    path('post/<id>/delete/', post, name="post-delete"),
+    path('post/<id>/update/', post_update, name="post-update"),
+    path('post/<id>/delete/', post_delete, name="post-delete"),
     path('tinymce/', include('tinymce.urls')),
 ]
 
